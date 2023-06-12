d3d5f071c69832235fcd0e260f477037a295ba78
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-13 00:35:45 +0400

    refactor class HasLinkFilter

--- filters/find_link.py
+++ filters/find_link.py
@@ -6,11 +6,11 @@ class HasLinkFilter(BaseFilter):
     async def __call__(self, message: Message):
         entities = message.entities or []
 
-        found_links = [
+        found_link = [
             item.extract_from(message.text) for item in message.entities
             if item.url is not None
         ]
 
         if len(entities) > 0:
-            return True
+            return {'find link': found_link}
         return False
