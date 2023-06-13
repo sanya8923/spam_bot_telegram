2cf0690e5a8cff73654f9926e859884210e752e9
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-13 19:49:46 +0400

    add filter find_new_member.py

--- handlers/save_chat_member_updated.py
+++ handlers/save_chat_member_updated.py
@@ -1,5 +1,11 @@
 from aiogram.types import Message, ChatMemberUpdated
+from aiogram import Router
+
+
+router = Router()
 
 
 async def save_chat_member_updated(message: Message):
-    pass
+    member_updated = {}
+    new_chat_member = message.new_chat_members
+
