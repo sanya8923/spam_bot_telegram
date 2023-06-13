0da9e2e845f44668452c8ed274d6579d5b60d453
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-14 00:46:56 +0400

    add message_fields.py

--- handlers/posting_too_often.py
+++ handlers/posting_too_often.py
@@ -11,3 +11,4 @@ async def posting_too_often(message: Message):
     user_id = message.from_user.id
     date_message = message.date
     print(f'user {user_id} send message {date_message}')
+    print(message.json(indent=4))
--- handlers/save_chat_member_updated.py
+++ handlers/save_chat_member_updated.py
@@ -1,13 +1,20 @@
 from aiogram.types import ChatMemberUpdated
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, IS_MEMBER, IS_NOT_MEMBER
 from aiogram import Router
+from aiogram.types.chat_join_request import ChatJoinRequest
 
 
 router = Router()
 
 
-@router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+# @router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+@router.chat_member()
 async def on_chat_member_update(event: ChatMemberUpdated):
     print(f'new member: {event.from_user.id}')
 
+@router.chat_join_request()
+async def on_chat_join_request(event: ChatJoinRequest):
+    print(f'chat join request', event)
 
+
+# @router.chat_member(ChatMemberUpdatedFilter())
