7474d8bd5492eed71d4f2598507329cf57dd1407
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-15 21:08:36 +0400

    refactor on_chat_member_update (add data_join)

--- handlers/save_chat_member_updated.py
+++ handlers/save_chat_member_updated.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.types import Message
+import datetime
 
 
 router = Router()
@@ -9,18 +10,19 @@ router = Router()
 async def on_chat_member_update(message: Message):
     keys = ['user_id', 'chat_id', 'date_join', 'date_message']
 
-    if message.new_chat_members is not None:
-        data_join = message.date
+    # if message.new_chat_members is not None:
+    #     data_join = datetime(message.date)
 
-    # member_data = {user_id: message.from_user.id,
-    #                chat_id: message.chat.id,
-    #                data_join: lambda (mess)
-    #                data_message: message.date
-    #                 }
+    member_data = {'user_id': message.from_user.id,
+                   'chat_id': message.chat.id,
+                   'data_join': message.new_chat_members,
+                   'data_message': message.date
+                    }
 #
 #     mem
 #
 #     member_data = {key: json_data.get(key) for key in keys}
-#     print(member_data)
+    for keys, value in member_data.items():
+        print(member_data)
 
 
