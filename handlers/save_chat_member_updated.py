1e9fae21ebb6086d45131b63705fba770b9198f5
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-15 21:34:37 +0400

    refactor on_chat_member_update (add to member_data the join_message)

--- handlers/save_chat_member_updated.py
+++ handlers/save_chat_member_updated.py
@@ -8,24 +8,19 @@ router = Router()
 
 @router.message()
 async def on_chat_member_update(message: Message):
-    keys = ['user_id', 'chat_id', 'date_join', 'date_message']
+    member_data = {'user_id': message.from_user.id,
+                   'chat_id': message.chat.id,
+                   'data_message': message.date
+                   }
 
     data_join = message.new_chat_members
     data_join_flag = False
     if data_join is not None:
         data_join_flag = True
 
-    member_data = {'user_id': message.from_user.id,
-                   'chat_id': message.chat.id,
-                   'data_join': message.new_chat_members,
-                   'data_message': message.date
-                    }
-#
-#     mem
-#
-#     member_data = {key: json_data.get(key) for key in keys}
-#     for keys, value in member_data.items():
-#         print(keys, value)
-    print(data_join_flag)
+    member_data['join_message'] = data_join_flag
+
+    for keys, value in member_data.items():
+        print(keys, value)
 
 
