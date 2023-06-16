a0c520767502bc06fe8615a5ca79184a6e4505ad
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-17 00:46:55 +0400

    add func add_stop_words

--- handlers/add_stop_words.py
+++ handlers/add_stop_words.py
@@ -1,5 +1,16 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message
+from aiogram.filters import CommandObject, Command
 
 
 router = Router()
+router.message.filter(F.chat.type == 'private')
+
+stop_words = []
+
+
+@router.message(Command('add_stop_words'))
+async def add_stop_words(message: Message, command: CommandObject):
+    if command.args:
+        stop_words.extend(command.args.split())
+        await message.answer(f'Words {stop_words} added')
