b89b03127d6f4306cee9d2706b32b2a4e856237f
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-17 00:52:21 +0400

    refactor func add_stop_words

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import config_reader
 
 from aiogram import Bot, Dispatcher
 
-from handlers import bot_start, ban_new_user_for_links, save_message_update
+from handlers import bot_start, ban_new_user_for_links, add_stop_words
 from middlewares.save_message_update2 import SaveMessageUpdateMiddleware
 from middlewares.stop_words import DeleteMessageForStopWords
 
@@ -14,12 +14,13 @@ async def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=config_reader.config.bot_token.get_secret_value())
     dp = Dispatcher()
-    dp.message.middleware(DeleteMessageForStopWords())
+    # dp.message.middleware(DeleteMessageForStopWords())
     # dp.message.middleware(SaveMessageUpdateMiddleware())
 
     dp.include_routers(
         bot_start.router,
         ban_new_user_for_links.router,
+        add_stop_words.router,
         # message_fields.router,
     )
 
