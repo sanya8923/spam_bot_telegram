5395f0aae84159b465ae4194d600996533970ecc
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-18 00:51:09 +0400

    rename get_status_member to new_member_checkin and refactor it

--- handlers/get_status_member.py
+++ handlers/get_status_member.py
@@ -1,10 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from handlers.save_message_update import MessageUpdate
-from process_checking import check_members_data
-from filter.find_link import HasLinkFilter
+from handlers.save_message_update import members_data
 
 
 router = Router()
@@ -13,30 +9,20 @@ router = Router()
 DURATION_OF_NEW_USER_STATUS = 86400  # 24 hours
 
 
-class MessageCheck(StatesGroup):
-    membership_term_not_defined = State()
-    new_user = State()
-    normal_user = State()
-
-
 @router.message()
-async def get_status_member(message: Message, state: FSMContext):
+async def new_member_checkin(message: Message) -> Bool:
 
-    await state.update_data(message_date=message.date, user_id=message.from_user.id)
-    await message.answer('Hi')
-    member_data = await state.get_data()
     print('get_status_member')
 
     date_join = [line['date_message'] for line in members_data if
-                 line['user_id'] == member_data['user_id'] and
+                 line['user_id'] == message.from_user.id and
                  line['join_message'] is True]
 
     if len(date_join) > 0:
-        duration_of_membership = member_data['message_date'] - date_join
+        duration_of_membership = message.date - date_join
         if duration_of_membership <= DURATION_OF_NEW_USER_STATUS:
-            await state.set_state(MessageCheck.new_user)
-        else:
-            await state.set_state(MessageCheck.normal_user)
+            return True
+
 
 
 
