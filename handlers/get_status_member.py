db4184d1ce5ad899f8d5f2bff7336b6ee194c4a6
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-18 21:11:12 +0400

    refactor get_status_member.py: add checking user_id (working!!!)

--- handlers/get_status_member.py
+++ handlers/get_status_member.py
@@ -14,13 +14,19 @@ DURATION_OF_NEW_USER_STATUS = 86400  # 24 hours
 async def new_member_checkin(message: Message) -> bool:
 
     date_join = None
+    user_id = None
     for line in members_data:
         if line.join_message is True:
             date_join = line.date_message
+            user_id = line.user_id
 
     if date_join is not None:
         duration_of_membership = message.date - date_join
-        if duration_of_membership.total_seconds() <= DURATION_OF_NEW_USER_STATUS:
+        if (message.from_user.id == user_id) and (duration_of_membership.total_seconds() <= DURATION_OF_NEW_USER_STATUS):
+            print(f'New user: True')
             return True
+        else:
+            print(f'New user: False')
+            return False
 
 
