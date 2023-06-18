8634ad8da91d979dd071e4dcdc340665dc3a6d08
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-18 23:18:53 +0400

    rename ban_new_user_for_links.py to ban_member.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import asyncio
 
@@ -9,7 +10,7 @@ from aiogram.types import Message
 from handlers import bot_start
 from handlers.get_status_member import new_member_checkin
 from handlers.save_message_update import save_message_update
-from handlers.ban_new_user_for_links import ban_new_user_for_link
+from handlers.ban_member import ban_new_user_for_link
 from handlers.checking_for_url import checking_for_url
 
 
@@ -32,7 +33,15 @@ async def main():
         if user_check is True:
             url_presence = await checking_for_url(message)
             if url_presence is True:
-                print(f'link: True')
+                await message.delete()
+
+                current_date = datetime.datetime.now()
+                next_day = current_date + datetime.timedelta(days=1)
+
+                await bot.ban_chat_member(message.chat.id,
+                                          message.from_user.id,
+                                          until_date=next_day,
+                                          revoke_messages=False)
         else:
             print(f'user_check: False')
 
--- handlers/ban_member.py
+++ handlers/ban_member.py
@@ -19,17 +19,7 @@ router = Router()
 
 @router.message()
 async def ban_new_user_for_link(message: Message):
-    entities = message.entities or []
-
-    found_links = [
-        item.extract_from(message.text) for item in entities
-        if item.type == "url"
-    ]
-
-    print(f'found_links: {found_links}')
-
-    if len(found_links) > 0:
-        print('link_founded')
+    pass
 
 
 
