0860218e407c0aeee55484c82f8e4f9289fcd981
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-20 21:26:39 +0400

    add chat_type_filter.py

--- handlers_user_management/ban_member.py
+++ handlers_user_management/ban_member.py
@@ -2,17 +2,20 @@ from aiogram import Router, F
 from aiogram.types import Message
 import datetime
 
-from filter.find_link import HasLinkFilter
+from filter.find_link_filter import HasLinkFilter
 from handlers.save_message_update import members_data
 from middlewares.save_message_update2 import SaveMessageUpdateMiddleware
 
 router = Router()
+banned_members = []
 
 
 @router.message()
 async def ban_member(bot, message: Message, ban_duration_min: int) -> None:
     current_date = datetime.datetime.now()
     next_day = current_date + datetime.timedelta(minutes=ban_duration_min)
+
+    banned_members.append(message.from_user.id)
 
     await bot.ban_chat_member(message.chat.id,
                               message.from_user.id,
