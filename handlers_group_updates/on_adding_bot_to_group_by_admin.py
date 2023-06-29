b9dd4f1b2ec4fe2a5544e3d1d7dc8398c5071390
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-06-30 00:15:49 +0400

    add func on_adding_bot_to_group_by_admin

--- handlers_group_updates/on_adding_bot_to_group_by_admin.py
+++ handlers_group_updates/on_adding_bot_to_group_by_admin.py
@@ -1,5 +1,8 @@
 from aiogram.types import ChatMemberUpdated
+from db.db_mongodb import add_group_id_where_bot_is_admin
 
 
 async def on_adding_bot_to_group_by_admin(update: ChatMemberUpdated):
     print('bot added to group - admin')
+    chat_id = update.chat.id
+    await add_group_id_where_bot_is_admin(chat_id)
