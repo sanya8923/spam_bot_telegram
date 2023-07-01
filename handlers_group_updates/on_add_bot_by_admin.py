eb59ddf62a173a56653e66097aa909792b81126e
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-01 23:33:21 +0400

    Revert "Revert "refactor on_adding_bot_to_group_by_admin""
    This reverts commit e4ba950275ead53d328941d1c4726ea9cf4f991c.

--- handlers_group_updates/on_add_bot_by_admin.py
+++ handlers_group_updates/on_add_bot_by_admin.py
@@ -1,10 +1,12 @@
 from aiogram.types import ChatMemberUpdated
 from db.db_mongodb import remove_group_id_where_bot_is_no_longer_member
 from handlers_group_management_user.save_group_admins import save_group_admins
+from handlers_group.save_group import save_group
 
 
-async def on_adding_bot_to_group_by_admin(update: ChatMemberUpdated):
+async def on_add_bot_by_admin(update: ChatMemberUpdated):
     print('bot added to group - admin')
-    chat_id = update.chat.id
-    await save_group_admins(chat_id)
+
+    await save_group_admins(update.chat.id)
+    await save_group(update)
     await remove_group_id_where_bot_is_no_longer_member(chat_id)
