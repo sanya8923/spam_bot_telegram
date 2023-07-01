a4f5d07ecd1e92792489d8ba24aae498aada0510
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-01 23:30:30 +0400

    refactor save_group_admins

--- handlers_group_management_user/save_group_admins.py
+++ handlers_group_management_user/save_group_admins.py
@@ -3,19 +3,31 @@ from db.db_mongodb import db, add_data_to_db
 
 
 async def save_group_admins(chat_id):
-    collection_name = 'group_user_role'
     admins_data = await bot.get_chat_administrators(chat_id)
+    collection_user = db['users']
 
     for line in admins_data:
         if line.status == 'administrator':
             admin = {'user_id': line.user.id,
+                     'username': line.user.username,
                      'chat_id': chat_id,
                      'role': 'administrator'
                      }
-            await add_data_to_db(collection_name, admin)
+            await add_data_to_db('group_user_role', admin)
         elif line.status == 'creator':
             creator = {'user_id': line.user.id,
+                       'username': line.user.username,
                        'chat_id': chat_id,
                        'role': 'creator'
                        }
-            await add_data_to_db(collection_name, creator)
+            await add_data_to_db('group_user_role', creator)
+
+        count = await collection_user.count_documents({'user_id': line.user.id})
+
+        if count == 0:
+            user = {'user_id': line.user.id,
+                    'username': line.user.username,
+                    'first_name': line.user.first_name,
+                    'last_name': line.user.last_name
+                    }
+            await add_data_to_db('users', user)
