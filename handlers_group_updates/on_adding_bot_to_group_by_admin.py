07322db0b54ef0c4a915f938b3a7db346c6466dc
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-01 22:34:59 +0400

    refactor on_adding_bot_to_group_by_admin

--- handlers_group_updates/on_adding_bot_to_group_by_admin.py
+++ handlers_group_updates/on_adding_bot_to_group_by_admin.py
@@ -1,10 +1,10 @@
 from aiogram.types import ChatMemberUpdated
 from db.db_mongodb import remove_group_id_where_bot_is_no_longer_member
+from handlers_group_management_user.save_group_admins import save_group_admins
 
 
 async def on_adding_bot_to_group_by_admin(update: ChatMemberUpdated):
     print('bot added to group - admin')
-    # print(update.json(indent=4))
     chat_id = update.chat.id
-    # await add_chat_id_to_collection(chat_id, True)
+    await save_group_admins(chat_id)
     await remove_group_id_where_bot_is_no_longer_member(chat_id)
