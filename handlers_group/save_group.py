fec3dd8e202f26d516f068adcedc62dfd632b001
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-01 23:30:00 +0400

    refactor save_group

--- handlers_group/save_group.py
+++ handlers_group/save_group.py
@@ -1,16 +1,16 @@
-from aiogram.types import Message
+from aiogram.types import ChatMemberUpdated
 
 from db.db_mongodb import db, add_data_to_db
 
 
-async def save_group(message: Message):
+async def save_group(update: ChatMemberUpdated):
     collection_name = 'groups'
     collection = db[collection_name]
 
-    count = await collection.count_documents({'group_id': message.chat.id})
+    count = await collection.count_documents({'group_id': update.chat.id})
     if count == 0:
         group = {
-            'group_id': message.chat.id
+            'group_id': update.chat.id
         }
         await add_data_to_db(collection_name, group)
 
