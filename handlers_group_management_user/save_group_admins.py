12900150ce7da811983ffa351a03149f41aed5af
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-01 22:34:39 +0400

    add save_group_admins

--- handlers_group_management_user/save_group_admins.py
+++ handlers_group_management_user/save_group_admins.py
@@ -1,16 +1,21 @@
 from bot import bot
-from han
+from db.db_mongodb import db, add_data_to_db
 
 
 async def save_group_admins(chat_id):
+    collection_name = 'group_user_role'
     admins_data = await bot.get_chat_administrators(chat_id)
 
     for line in admins_data:
         if line.status == 'administrator':
-            print(f'id: {line.user.id}')
-            print(f'username: {line.user.username}')
-            print(f'first_name: {line.user.first_name}')
-            print(f'last_name: {line.user.last_name}')
-
-
-
+            admin = {'user_id': line.user.id,
+                     'chat_id': chat_id,
+                     'role': 'administrator'
+                     }
+            await add_data_to_db(collection_name, admin)
+        elif line.status == 'creator':
+            creator = {'user_id': line.user.id,
+                       'chat_id': chat_id,
+                       'role': 'creator'
+                       }
+            await add_data_to_db(collection_name, creator)
