47ef9d3521994afed14918ca3c07506348a82016
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-03 00:23:36 +0400

    refactor on_new_message_group_supergroup

--- handlers_group/on_new_message_group_supergroup.py
+++ handlers_group/on_new_message_group_supergroup.py
@@ -7,7 +7,7 @@ from handlers_group_management_user.get_member_status_group_supergroup import ge
 from handlers_group.on_new_message_from_creator import on_new_message_from_creator
 from handlers_group.on_new_message_from_admin import on_new_message_from_admin
 from handlers_group.on_new_message_from_member import on_new_message_from_member
-from handlers_group_management_user.update_users_db import save_user
+from handlers_group_management_user.update_users_db_from_message import update_users_db_from_message
 from handlers_group_management_message.save_message_update import save_message_update
 from handlers_group.save_group import save_group
 
@@ -18,7 +18,7 @@ router = Router()
 @router.message()
 async def on_new_message_group_supergroup(message: Message) -> Coroutine:
     await save_group(message)
-    await save_user(message)
+    await update_users_db_from_message(message)
     await save_message_update(message)
 
     chat_member_status = await get_member_status_group_supergroup(message)
