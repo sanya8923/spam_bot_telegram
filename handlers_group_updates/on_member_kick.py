db667609fe6c0ff8a4b92487914dd1cff03443bb
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-03 00:48:58 +0400

    refactor on_member_kick

--- handlers_group_updates/on_member_kick.py
+++ handlers_group_updates/on_member_kick.py
@@ -1,5 +1,6 @@
 from aiogram.types import ChatMemberUpdated
+from db.update_group_user_role_db import update_group_user_role_db
 
 
 async def on_member_kick(update: ChatMemberUpdated):
-    pass
+    await update_group_user_role_db(update)
