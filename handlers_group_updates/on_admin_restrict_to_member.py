d2869ab39f1c5435d4e4b2dda521992d5ff8daca
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-03 00:25:17 +0400

    refactor on_admin_restrict_to_member

--- handlers_group_updates/on_admin_restrict_to_member.py
+++ handlers_group_updates/on_admin_restrict_to_member.py
@@ -1,5 +1,6 @@
 from aiogram.types import ChatMemberUpdated
+from handlers_group_updates.update_group_user_role_db import update_group_user_role_db
 
 
 async def on_admin_restrict_to_member(update: ChatMemberUpdated):
-    pass
+    await update_group_user_role_db(update)
--- handlers_group_updates/on_member_new.py
+++ handlers_group_updates/on_member_new.py
@@ -1,9 +1,9 @@
 from aiogram.types import ChatMemberUpdated
-from handlers_group_management_user.update_users_db import update_users_db
+from handlers_group_management_user.update_users_db_from_member_update import update_users_db_from_member_update
 from handlers_group_updates.update_group_user_role_db import update_group_user_role_db
 
 
 async def on_member_new(update: ChatMemberUpdated):
     print('on_new_member')
-    await update_users_db(update)
+    await update_users_db_from_member_update(update)
     await update_group_user_role_db(update)
