18a5fe1b27cbcc59845b1ca3763922af914f7d01
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-02 23:46:38 +0400

    refactor update_group_user_role_db

--- handlers_group_updates/update_group_user_role_db.py
+++ handlers_group_updates/update_group_user_role_db.py
@@ -1,14 +1,37 @@
 from aiogram.types import ChatMemberUpdated
 from db.db_mongodb import db
+from db.db_mongodb import add_data_to_db
 
 
-async def update_group_user_role_db(update: ChatMemberUpdated, role: str):
-    chat_id = update.chat.id
-    user_id = update.from_user.id
+async def update_group_user_role_db(update: ChatMemberUpdated):
 
     collection_name = 'group_user_role'
     collection = db[collection_name]
+    count = await collection.count_documents({'user_id': update.new_chat_member.user.id, 'chat_id': update.chat.id})
 
-    print('update_group_user_role_db')
+    if count == 0:
+        group_user_role = {'user_id': update.new_chat_member.user.id,
+                           'username': update.new_chat_member.user.username,
+                           'chat_id': update.chat.id,
+                           'role': update.new_chat_member.status
+                           }
+        await add_data_to_db(collection_name, group_user_role)
+    elif count == 1:
+        filter_update = {'user_id': update.new_chat_member.user.id, 'chat_id': update.chat.id}
+        update = {'$set': {'role': update.new_chat_member.status}}
+        collection.update_one(filter_update, update)
+    else:
+        delete_filter = {'user_id': update.new_chat_member.user.id,
+                         'chat_id': update.chat.id
+                         }
+        collection.delete_many(delete_filter)
 
-    collection.update_one({'user_id': user_id, 'chat_id': chat_id}, {'$set': {'role': role}})
+        group_user_role = {'user_id': update.new_chat_member.user.id,
+                           'username': update.new_chat_member.user.username,
+                           'chat_id': update.chat.id,
+                           'role': update.new_chat_member.status
+                           }
+        await add_data_to_db(collection_name, group_user_role)
+
+
+
