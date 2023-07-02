d4e9002066975d42767e05b3ce7f5f987f483d9c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-02 23:47:30 +0400

    refactor on_admin_new

--- handlers_group_updates/on_admin_new.py
+++ handlers_group_updates/on_admin_new.py
@@ -4,4 +4,4 @@ from handlers_group_updates.update_group_user_role_db import update_group_user_r
 
 async def on_admin_new(update: ChatMemberUpdated):
     print('on_new_admin')
-    await update_group_user_role_db(update, 'administrator')
+    await update_group_user_role_db(update)
--- handlers_group_updates/on_member_new.py
+++ handlers_group_updates/on_member_new.py
@@ -1,7 +1,9 @@
 from aiogram.types import ChatMemberUpdated
 from handlers_group_management_user.save_user import save_user
+from handlers_group_management_user.update_group_user_role import update_group_user_role
 
 
 async def on_member_new(update: ChatMemberUpdated):
     print('on_new_member')
     await save_user(update)
+    await update_group_user_role(update, 'member')
