d69763d2791285273f3f9cfa11898c4b777b0122
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-03 01:07:20 +0400

    rename save_group_admins.py -> save_admins_to_db.py

--- db/save_admins_to_db.py
+++ db/save_admins_to_db.py
@@ -1,23 +1,25 @@
+from aiogram.types import ChatMemberUpdated
 from bot import bot
 from db.db_mongodb import db, add_data_to_db
 
 
-async def save_group_admins(chat_id):
-    admins_data = await bot.get_chat_administrators(chat_id)
+async def save_admins_to_db(update: ChatMemberUpdated):
+    admins_data = await bot.get_chat_administrators(update.chat.id)
+
     collection_user = db['users']
 
     for line in admins_data:
         if line.status == 'administrator':
             admin = {'user_id': line.user.id,
                      'username': line.user.username,
-                     'chat_id': chat_id,
+                     'chat_id': update.chat.id,
                      'role': 'administrator'
                      }
             await add_data_to_db('group_user_role', admin)
         elif line.status == 'creator':
             creator = {'user_id': line.user.id,
                        'username': line.user.username,
-                       'chat_id': chat_id,
+                       'chat_id': update.chat.id,
                        'role': 'creator'
                        }
             await add_data_to_db('group_user_role', creator)
--- handlers_group_updates/on_bot_add_by_admin.py
+++ handlers_group_updates/on_bot_add_by_admin.py
@@ -1,11 +1,10 @@
 from aiogram.types import ChatMemberUpdated
-from db.save_group_admins import save_group_admins
+from db.save_admins_to_db import save_admins_to_db
 from db.save_group_to_db import save_group_to_db
 
 
 async def on_bot_add_by_admin(update: ChatMemberUpdated):
     print('bot added to group - admin')
 
-    await save_group_admins(update.chat.id)
+    await save_admins_to_db(update)
     await save_group_to_db(update)
-    # await remove_group_id_where_bot_is_no_longer_member(chat_id)
