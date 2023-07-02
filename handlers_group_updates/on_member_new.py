9d289367632665db828d7bef17b6da09ea4f43de
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-02 23:50:18 +0400

    refactor on_member_new

--- handlers_group_updates/on_member_new.py
+++ handlers_group_updates/on_member_new.py
@@ -1,9 +1,9 @@
 from aiogram.types import ChatMemberUpdated
 from handlers_group_management_user.save_user import save_user
-from handlers_group_management_user.update_group_user_role import update_group_user_role
+from handlers_group_updates.update_group_user_role_db import update_group_user_role_db
 
 
 async def on_member_new(update: ChatMemberUpdated):
     print('on_new_member')
     await save_user(update)
-    await update_group_user_role(update, 'member')
+    await update_group_user_role_db(update)
