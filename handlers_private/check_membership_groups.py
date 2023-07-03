3387203145127052ea2ff4b079619cffe96d3e2c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-03 23:09:51 +0400

    refactor check_membership_groups.py

--- handlers_private/check_membership_groups.py
+++ handlers_private/check_membership_groups.py
@@ -5,17 +5,16 @@ from keyboards.choice_groups_keyboard import choice_groups_keyboard
 
 async def check_membership_groups(update: Message):
     print('check_membership_groups')
-    user_id = update.from_user.id
 
     collection = db['group_user_role']
-
-    documents = collection.find({'user_id': user_id})
-
-
+    documents = collection.find({'user_id': update.from_user.idd})
     documents_as_list = await documents.to_list(length=100)
+
+    chat_names = []
     for document in documents_as_list:
         if document['role'] == 'creator' or 'administrator':
-            groups_id = document['chat_id']
+            chat_names.append(document['chat_name'])
+    return await choice_groups_keyboard(chat_names)
 
 
 
