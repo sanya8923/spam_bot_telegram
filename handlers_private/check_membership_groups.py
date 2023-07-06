7a10d29f2050fd3a780e92d49ebf25a12d20a8f8
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-06 22:35:42 +0400

    refactor check_membership_groups.py

--- handlers_private/check_membership_groups.py
+++ handlers_private/check_membership_groups.py
@@ -6,15 +6,24 @@ from keyboards.choice_groups_keyboard import choice_groups_keyboard
 async def check_membership_groups(update: Message):
     print('check_membership_groups')
 
-    collection = db['group_user_role']
-    documents = collection.find({'user_id': update.from_user.id})
-    documents_as_list = await documents.to_list(length=100)
+    collection_group_user_role = db['group_user_role']
+    documents_group_user_role = collection_group_user_role.find({'user_id': update.from_user.id})
 
-    chat_names = []
-    for document in documents_as_list:
-        if document['role'] == 'creator' or 'administrator':
-            chat_names.append(document['chat_name'])
-    return await choice_groups_keyboard(update, chat_names)
+    chat_id_list = []
+    async for document in documents_group_user_role:
+        chat_id_list.append(document['chat_id'])
+
+    collection_groups = db['groups']
+    chat_data = []
+
+    for chat_id in chat_id_list:
+        documents_groups = collection_groups.find({f'chat_id': chat_id})
+        async for document in documents_groups:
+            chat_data.append((document['chat_name'], document['chat_username']))
+            print(f'chat_data: {chat_data}')
+            await choice_groups_keyboard(update, chat_data)
+
+
 
 
 
