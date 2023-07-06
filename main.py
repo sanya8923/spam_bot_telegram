128c6a293e252bce9d9d133f1aff8f432a796ecb
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-07 01:00:44 +0400

    remove unnecessary features to folder other

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import asyncio
 from bot import bot
 
 from aiogram import Dispatcher
-from handlers_group import on_new_message_private, on_new_message_group_supergroup
+from handlers_group import on_new_message_private, on_new_message_from_member, on_new_message_from_admin, \
+    on_new_message_from_creator
 from handlers_group_updates import on_update_my_chat_member, on_update_chat_member
-# from middlewares.get_chat_type_middleware_outer import GetChatTypeMiddlewareOuter
 
 
 async def main():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     dp = Dispatcher()
-    # dp.message.middleware(GetChatTypeMiddlewareOuter())
 
     dp.include_routers(
         on_update_my_chat_member.router,
         on_update_chat_member.router,
         on_new_message_private.router,
-        on_new_message_group_supergroup.router,
+        on_new_message_from_admin.router,
+        on_new_message_from_creator.router,
+        on_new_message_from_member.router
     )
 
     await bot.delete_webhook(drop_pending_updates=True)
--- other/on_new_message_group_supergroup.py
+++ other/on_new_message_group_supergroup.py
@@ -4,9 +4,9 @@ from typing import Coroutine
 
 from handlers_group_management_user.get_member_status_group_supergroup import get_member_status_group_supergroup
 
-from handlers_group.on_new_message_from_creator import on_new_message_from_creator
-from handlers_group.on_new_message_from_admin import on_new_message_from_admin
-from handlers_group.on_new_message_from_member import on_new_message_from_member
+from handlers_group.on_new_message_from_creator import on_new_message_from_creator_group
+from handlers_group.on_new_message_from_admin import on_new_message_from_admin_group
+from handlers_group.on_new_message_from_member import on_new_message_from_member_group
 from db.update_users_db_from_message import update_users_db_from_message
 from db.save_message_update import save_message_update
 from db.save_group_to_db import save_group_to_db
@@ -26,9 +26,9 @@ async def on_new_message_group_supergroup(message: Message) -> Coroutine:
 
     chat_member_status = await get_member_status_group_supergroup(message)
     if chat_member_status == 'creator':
-        return await on_new_message_from_creator(message)
+        return await on_new_message_from_creator_group(message)
     elif chat_member_status == 'administrator':
-        return await on_new_message_from_admin(message)
+        return await on_new_message_from_admin_group(message)
     elif chat_member_status == 'member':
-        return await on_new_message_from_member(message)
+        return await on_new_message_from_member_group(message)
 
