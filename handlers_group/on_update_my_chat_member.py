97ca625be506cfd853d8c1875abf9873df075790
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-07 18:55:03 +0400

    refactor on_update_my_chat_member.py

--- handlers_group/on_update_my_chat_member.py
+++ handlers_group/on_update_my_chat_member.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION, PROMOTED_TRANSITION, \
-    LEAVE_TRANSITION, IS_ADMIN, IS_MEMBER
+    LEAVE_TRANSITION, IS_ADMIN, MEMBER
 from aiogram.types import ChatMemberUpdated
 
 from db.save_admins_to_db import save_admins_to_db
@@ -25,7 +25,7 @@ async def on_add_bot_by_admin(update: ChatMemberUpdated):
     await save_group_to_db(update)
 
 
-@router.my_chat_member(ChatMemberUpdatedFilter(IS_ADMIN >> IS_MEMBER))
+@router.my_chat_member(ChatMemberUpdatedFilter(IS_ADMIN >> MEMBER))
 async def on_downgrade_bot(update: ChatMemberUpdated):
     print('bot downgrade from admin to member')
 
