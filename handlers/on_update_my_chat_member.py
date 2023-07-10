0f82757c90b4dc2a934e0bee1191f0338a700a18
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-10 20:45:44 +0400

    refactor on_update_my_chat_member.py (in on_leave_bot_from_group switch try_except to supress) not working

--- handlers/on_update_my_chat_member.py
+++ handlers/on_update_my_chat_member.py
@@ -1,14 +1,17 @@
 from aiogram import Router
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION, PROMOTED_TRANSITION, \
-    LEAVE_TRANSITION, IS_ADMIN, MEMBER, KICKED
+    LEAVE_TRANSITION, IS_ADMIN, MEMBER
 from aiogram.types import ChatMemberUpdated
 
 from db.save_admins_to_db import save_admins_to_db
 from db.save_group_to_db import save_group_to_db
 
 from filter.chat_type_filter import ChatTypeFilter
-from aiogram.exceptions import TelegramForbiddenError
+
 from db.delete_chat_id_from_groups_db import delete_chat_id_from_groups_db
+
+from aiogram.exceptions import TelegramForbiddenError
+from contextlib import suppress
 
 
 router = Router()
@@ -35,10 +38,9 @@ async def on_downgrade_bot(update: ChatMemberUpdated):
 
 @router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=LEAVE_TRANSITION))
 async def on_leave_bot_from_group(update: ChatMemberUpdated):
-    try:
+    with suppress(TelegramForbiddenError):
         print('bot leave the group')
         await delete_chat_id_from_groups_db(update)
-    except TelegramForbiddenError as e:
-        print('TelegramForbiddenError:', e)
+
 
 
