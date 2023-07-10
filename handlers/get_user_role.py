76ccff8fce3efa2085ec1a62c31f31e712451809
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-11 02:09:19 +0400

    refactor get_user_role.py

--- handlers/get_user_role.py
+++ handlers/get_user_role.py
@@ -3,4 +3,6 @@ from aiogram.types import Message
 
 
 async def get_user_role(message: Message):
-    return bot.get_chat_member(message.chat.id, message.from_user.id)
+    print('get_user_role')
+    user = await bot.get_chat_member(message.chat.id, message.from_user.id)
+    return user.status
