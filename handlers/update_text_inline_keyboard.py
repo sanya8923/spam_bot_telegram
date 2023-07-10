5b7759704d20cbd81e2ad952769f2cda211ee50e
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-10 22:55:38 +0400

    refactor update_text_inline_keyboard.py (add stipulation if pattern 'update_membership_groups' and not chats with botello - await button_update_groups_list again)

--- handlers/update_text_inline_keyboard.py
+++ handlers/update_text_inline_keyboard.py
@@ -3,11 +3,12 @@ from aiogram.exceptions import TelegramBadRequest
 from contextlib import suppress
 from keyboards.management_group_inline_keyboard import group_management_inline_keyboard
 from keyboards.choice_groups_inline_keyboard import choice_groups_inline_keyboard
-from texts_of_message import text_check_membership, text_choice_group
+from keyboards.button_update_groups_list import button_update_groups_list
+from texts_of_message import text_check_membership, text_choice_group, text_not_group
 from handlers.check_membership_groups import check_membership_groups
 
 
-async def update_text_inline_keyboard(message: Message, chat_id: int, user_id: int, pattern: str):
+async def update_text_inline_keyboard(message: Message, chat_id: int, user_id: int, pattern: str):  # TODO: add args
     with suppress(TelegramBadRequest):
         print('text_inline_keyboard')
         if pattern == 'group_management':
@@ -15,5 +16,9 @@ async def update_text_inline_keyboard(message: Message, chat_id: int, user_id: i
                                     reply_markup=group_management_inline_keyboard(chat_id))
         elif pattern == 'update_membership_groups':
             chat_data = await check_membership_groups(user_id)
-            await message.edit_text(text_check_membership,
-                                    reply_markup=choice_groups_inline_keyboard(message.from_user.id, chat_data))
+            if len(chat_data) > 0:
+                await message.edit_text(text_check_membership,
+                                        reply_markup=choice_groups_inline_keyboard(message.from_user.id, chat_data))
+            else:
+                await message.edit_text(text_not_group,
+                                        reply_markup=button_update_groups_list(message.from_user.id))
