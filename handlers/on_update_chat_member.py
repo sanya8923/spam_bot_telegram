3059a62e7047d3ca64a44141c5304227281d77ce
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-11 02:11:41 +0400

    refactor on_update_chat_member.py (rename update_group_user_role_db to update_group_user_role_db_from_member_update)

--- handlers/on_update_chat_member.py
+++ handlers/on_update_chat_member.py
@@ -3,7 +3,7 @@ from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, ChatMem
     PROMOTED_TRANSITION, IS_ADMIN, MEMBER, IS_MEMBER, LEFT, KICKED
 from filter.chat_type_filter import ChatTypeFilter
 from db.update_users_db_from_member_update import update_users_db_from_member_update
-from db.update_group_user_role_db import update_group_user_role_db
+from db.update_group_user_role_db_from_member_update import update_group_user_role_db_from_member_update
 
 
 router = Router()
@@ -14,30 +14,30 @@ router.message.filter(ChatTypeFilter(chat_type=['group', 'supergroup']))
 async def on_new_member(update: ChatMemberUpdated):
     print('on_new_member')
     await update_users_db_from_member_update(update)
-    await update_group_user_role_db(update)
+    await update_group_user_role_db_from_member_update(update)
 
 
 @router.chat_member(ChatMemberUpdatedFilter(member_status_changed=PROMOTED_TRANSITION))
 async def on_admin_new(update: ChatMemberUpdated):
     print('on_new_admin')
     await update_users_db_from_member_update(update)
-    await update_group_user_role_db(update)
+    await update_group_user_role_db_from_member_update(update)
 
 
 @router.chat_member(ChatMemberUpdatedFilter(IS_ADMIN >> MEMBER))
 async def on_admin_downgrade_to_member(update: ChatMemberUpdated):
     print('on_admin_downgrade_to_member')
-    await update_group_user_role_db(update)
+    await update_group_user_role_db_from_member_update(update)
 
 
 @router.chat_member(ChatMemberUpdatedFilter(IS_MEMBER >> LEFT))
 async def on_admin_member_left(update: ChatMemberUpdated):
     print('on_admin_member_left')
-    await update_group_user_role_db(update)
+    await update_group_user_role_db_from_member_update(update)
 
 
 @router.chat_member(ChatMemberUpdatedFilter(IS_MEMBER >> KICKED))
 async def on_admin_member_kick(update: ChatMemberUpdated):
     print('on_admin_member_kick')
-    await update_group_user_role_db(update)
+    await update_group_user_role_db_from_member_update(update)
 
