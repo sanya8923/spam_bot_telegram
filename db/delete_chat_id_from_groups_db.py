2536711c2bf4b29740bec8b28222cf478321936e
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-10 20:44:30 +0400

    refactor delete_chat_id_from_groups_db.py (not working)

--- db/delete_chat_id_from_groups_db.py
+++ db/delete_chat_id_from_groups_db.py
@@ -4,14 +4,14 @@ from aiogram.types import ChatMemberUpdated
 
 async def delete_chat_id_from_groups_db(update: ChatMemberUpdated):
     print('delete_chat_id_from_groups_db')
-    collection_name = 'groups'
-    collection = db[collection_name]
-
-    count = collection.count_documents({'chat_id': update.chat.id})
-
-    if count == 1:
-        collection.delete_one({'chat_id': update.chat.id})
-    elif count > 1:
-        collection.delete_many({'chat_id': update.chat.id})
-    else:
-        raise ValueError("Chat ID don't exists")
+    # collection_name = 'groups'
+    # collection = db[collection_name]
+    #
+    # count = collection.count_documents({'chat_id': update.chat.id})
+    #
+    # if count == 1:
+    #     collection.delete_one({'chat_id': update.chat.id})
+    # elif count > 1:
+    #     collection.delete_many({'chat_id': update.chat.id})
+    # else:
+    #     raise ValueError("Chat ID don't exists")
