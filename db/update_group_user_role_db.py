1e478a9cc0d6c6d4c3a71f53ccb44db8fd4fd85b
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-11 02:07:12 +0400

    refactor update_group_user_role_db.py

--- db/update_group_user_role_db.py
+++ db/update_group_user_role_db.py
@@ -1,33 +1,35 @@
-from aiogram.types import ChatMemberUpdated
+from aiogram.types import Message
+from handlers.get_user_role import get_user_role
 from db.db_mongodb import db, add_data_to_db
 
 
-async def update_group_user_role_db(update: ChatMemberUpdated):
-    # TODO: проверь возможно задваивает админа (или что-то типа того)
+async def update_group_user_role_db(message: Message):
     print('update_group_user_role_db')
     collection_name = 'group_user_role'
     collection = db[collection_name]
-    count = await collection.count_documents({'user_id': update.new_chat_member.user.id, 'chat_id': update.chat.id})
-    group_user_role = {'user_id': update.new_chat_member.user.id,
-                       'username': update.new_chat_member.user.username,  # TODO: delete
-                       'chat_id': update.chat.id,
-                       'chat_name': update.chat.title,  # TODO: delete
-                       'role': update.new_chat_member.status
+    role = await get_user_role(message)
+    count = await collection.count_documents({'user_id': message.from_user.id, 'chat_id': message.chat.id})
+
+    if await collection.find_one({'user_id': message.from_user.id, 'chat_id': message.chat.id}):
+        role_from_db = (await collection.find_one({'user_id': message.from_user.id, 'chat_id': message.chat.id}))['role']
+
+    group_user_role = {'user_id': message.from_user.id,
+                       'username': message.from_user.username,  # TODO: delete
+                       'chat_id': message.chat.id,
+                       'chat_name': message.chat.title,  # TODO: delete
+                       'role': role
                        }
 
     if count == 0:
+        print('new member add db')
         await add_data_to_db(collection_name, group_user_role)
-    elif count == 1:
-        filter_update = {'user_id': update.new_chat_member.user.id, 'chat_id': update.chat.id}
-        update = {'$set': {'role': update.new_chat_member.status}}
+    elif count == 1 and role != role_from_db:
+        print('update data in db')
+        filter_update = {'user_id': message.from_user.id, 'chat_id': message.chat.id}
+        update = {'$set': {'role': role}}
         collection.update_one(filter_update, update)
     else:
-        delete_filter = {'user_id': update.new_chat_member.user.id,
-                         'chat_id': update.chat.id
-                         }
-        collection.delete_many(delete_filter)
-
-        await add_data_to_db(collection_name, group_user_role)
+        print('user in db and role is correct')
 
 
 
