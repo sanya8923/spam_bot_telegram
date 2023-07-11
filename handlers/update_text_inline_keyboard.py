b45a22fcd1b8413ca4c92e6761efcc2e156ded00
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-12 00:55:16 +0400

    refactor update_text_inline_keyboard.py (use kwargs)

--- handlers/update_text_inline_keyboard.py
+++ handlers/update_text_inline_keyboard.py
@@ -9,19 +9,27 @@ from texts_of_message import text_check_membership, text_choice_group, text_not_
 from handlers.check_membership_groups import check_membership_groups
 
 
-async def update_text_inline_keyboard(message: Message, chat_id: int, user_id: int, pattern: str):  # TODO: add args
+async def update_text_inline_keyboard(**kwargs):  # TODO: add args
+    pattern = kwargs.get('pattern')
+    message = kwargs.get('message')
+    chat_id = kwargs.get('chat_id')
+    user_id = kwargs.get('user_id')
+
     with suppress(TelegramBadRequest):
         print('text_inline_keyboard')
         if pattern == 'group_management':
             await message.edit_text(text_choice_group,
                                     reply_markup=group_management_inline_keyboard(chat_id, user_id))
+
         elif pattern == 'update_membership_groups':
             chat_data = await check_membership_groups(user_id)
             if len(chat_data) > 0:
                 await message.edit_text(text_check_membership,
                                         reply_markup=choice_groups_inline_keyboard(message.from_user.id, chat_data))
+
         elif pattern == 'management_members':
-            await message.edit_text(text_management_members, reply_markup=management_members_inline_keyboard(me))
+            await message.edit_text(text_management_members, reply_markup=management_members_inline_keyboard())
+
         else:
             await message.edit_text(text_not_group,
                                     reply_markup=button_update_groups_list(message.from_user.id))
