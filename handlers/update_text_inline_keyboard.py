9cf508e323cf21eeaad2981fb92f6a0ccafc1594
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-13 20:22:41 +0400

    refactor update_text_inline_keyboard.py rafactor patterns add if

--- handlers/update_text_inline_keyboard.py
+++ handlers/update_text_inline_keyboard.py
@@ -4,8 +4,8 @@ from contextlib import suppress
 from keyboards.management_group_inline_keyboard import group_management_inline_keyboard
 from keyboards.choice_groups_inline_keyboard import choice_groups_inline_keyboard
 from keyboards.button_update_groups_list import button_update_groups_list
-from keyboards.management_members_inline_keyboard import management_members_inline_keyboard
-from texts_of_message import text_check_membership, text_choice_group, text_not_group, text_management_members
+from keyboards.management_members_inline_keyboard import members_management_inline_keyboard
+from texts_of_message import text_check_membership, text_choice_group, text_not_group, text_members_management
 from handlers.check_membership_groups import check_membership_groups
 
 
@@ -16,20 +16,21 @@ async def update_text_inline_keyboard(**kwargs):
     user_id = kwargs.get('user_id')
 
     with suppress(TelegramBadRequest):
-        print('text_inline_keyboard')
         if pattern == 'group_management':
             await message.edit_text(text_choice_group,
                                     reply_markup=group_management_inline_keyboard(chat_id, user_id))
 
         elif pattern == 'update_membership_groups':
             chat_data = await check_membership_groups(user_id)
+
             if len(chat_data) > 0:
                 await message.edit_text(text_check_membership,
-                                        reply_markup=choice_groups_inline_keyboard(message.from_user.id, chat_data))
+                                        reply_markup=choice_groups_inline_keyboard(user_id, chat_data))
 
-        elif pattern == 'management_members':
-            await message.edit_text(text_management_members, reply_markup=management_members_inline_keyboard())
+        elif pattern == 'members_management':
+            await message.edit_text(text_members_management,
+                                    reply_markup=members_management_inline_keyboard(user_id))
 
         else:
             await message.edit_text(text_not_group,
-                                    reply_markup=button_update_groups_list(message.from_user.id))
+                                    reply_markup=button_update_groups_list(user_id))
