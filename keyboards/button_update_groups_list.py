fafe2ce76e4833041a67fa33b8aeae5adb816a2f
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-13 20:23:47 +0400

    refactor button_update_groups_list.py: change callback_data

--- keyboards/button_update_groups_list.py
+++ keyboards/button_update_groups_list.py
@@ -5,6 +5,6 @@ from aiogram.utils.keyboard import InlineKeyboardMarkup
 def button_update_groups_list(user_id):
     print('button_update_groups_list')
     button = [
-        [InlineKeyboardButton(text='Обновить список групп', callback_data=f'uid_{user_id}')]
+        [InlineKeyboardButton(text='Обновить список групп', callback_data=f'UpdGr_{user_id}')]
     ]
     return InlineKeyboardMarkup(inline_keyboard=button)
