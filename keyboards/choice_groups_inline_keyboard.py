0510690d9c232f0c2bad39a1b9848f8603ac96f7
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-13 20:24:32 +0400

    refactor choice_groups_inline_keyboard.py (chenge callback_data)

--- keyboards/choice_groups_inline_keyboard.py
+++ keyboards/choice_groups_inline_keyboard.py
@@ -9,10 +9,12 @@ def choice_groups_inline_keyboard(user_id, chat_names: list):
     chat_id = 2
 
     buttons = [
-        [InlineKeyboardButton(text=f'{item[chat_name]} (@{item[username]})', callback_data=f'cid_{item[chat_id]}')]
+        [InlineKeyboardButton(text=f'{item[chat_name]} (@{item[username]})',
+                              callback_data=f'GrMan_{user_id}_{item[chat_id]}')]
         for item in chat_names
     ]
-    buttons.append([InlineKeyboardButton(text='Обновить список групп', callback_data=f'uid_{user_id}')])
+    buttons.append([InlineKeyboardButton(text='Обновить список групп',
+                                         callback_data=f'UpdGr_{user_id}')])
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
