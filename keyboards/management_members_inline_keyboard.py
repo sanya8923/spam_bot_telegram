e942142b91c70fe49fc7b1d345c21a4ac89be34f
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-13 20:57:20 +0400

    refactor management_members_inline_keyboard.py (change callback_data for over buttons)

--- keyboards/management_members_inline_keyboard.py
+++ keyboards/management_members_inline_keyboard.py
@@ -6,11 +6,11 @@ def members_management_inline_keyboard(chat_id: int, user_id: int):
     print('members_management_inline_keyboard')
 
     buttons = [
-        [InlineKeyboardButton(text='Забанить участника', callback_data='ban_user')],
-        [InlineKeyboardButton(text='Разбанить участника', callback_data='unban_user')],
-        [InlineKeyboardButton(text='Замьютить участника', callback_data='mute_user')],
-        [InlineKeyboardButton(text='Размьютить участника', callback_data='unmute_user')],
-        [InlineKeyboardButton(text='Назад', callback_data=f'UpdGr_{user_id}')],
+        [InlineKeyboardButton(text='Забанить участника', callback_data=f'ban_user_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Разбанить участника', callback_data=f'unban_user_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Замьютить участника', callback_data=f'mute_user_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Размьютить участника', callback_data=f'unmute_user_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Назад', callback_data=f'GrMan_{user_id}_{chat_id}')],
         [InlineKeyboardButton(text='К списку групп', callback_data=f'UpdGr_{user_id}')],
     ]
 
