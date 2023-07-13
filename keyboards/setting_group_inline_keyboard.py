0718ffe43bef5e23072a39eb699b8e8c6a49b0e7
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-13 21:19:19 +0400

    add setting_group_inline_keyboard.py

--- keyboards/setting_group_inline_keyboard.py
+++ keyboards/setting_group_inline_keyboard.py
@@ -6,6 +6,13 @@ def setting_group_inline_keyboard(chat_id: int, user_id: int):
     print('setting_group_inline_keyboard')
 
     buttons = [
-        [InlineKeyboardButton(text='Управление бан-словами', callback_data='BanWords_')]
+        [InlineKeyboardButton(text='Управление бан-словами', callback_data=f'BanWords_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Срок статуса "новый участник"', callback_data=f'NewMembTerm_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Разрешить ссылки для новых участников', callback_data=f'NewMembLink_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Разрешить ссылки', callback_data=f'Link_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Разрешить пересылку сообщений из др. групп', callback_data=f'Forwarding_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Слишком частый постинг', callback_data=f'TooOftenPost_{user_id}_{chat_id}')],
     ]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
 
