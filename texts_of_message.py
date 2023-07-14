4c892dd8298f0cb9e8fbe3a1534b9947d23bb1d0
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-15 00:38:03 +0400

    refactor texts_of_message.py: add text_admin_try_ban_admin

--- texts_of_message.py
+++ texts_of_message.py
@@ -6,4 +6,6 @@ text_members_management = 'Выберите действие:'
 text_admins_management = 'Выберите действие:'
 text_setting_group = 'Выберите действие:'
 text_ban_user_from_private = 'Введите username пользователя, которого вы хотите забанить'
-text_user_banned_from_private = f'user is banned'
+text_user_banned_from_private = 'user is banned'
+text_admin_try_ban_admin = '''Этот пользователь является администратором группы. Администраторов имеет право банить 
+только владелец группы'''
