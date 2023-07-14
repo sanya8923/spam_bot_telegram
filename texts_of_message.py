484c542551131b3a5e7557a812d50b0ae8a0a328
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-15 00:54:59 +0400

    refactor texts_of_message.py: add text_banned_user_is_creator

--- texts_of_message.py
+++ texts_of_message.py
@@ -5,10 +5,13 @@ text_choice_group = 'Выберите действие:'
 text_members_management = 'Выберите действие:'
 text_admins_management = 'Выберите действие:'
 text_setting_group = 'Выберите действие:'
+
+# TEXTS BAN USER FROM PRIVATE
 text_ban_user_from_private = 'Введите username пользователя, которого вы хотите забанить'
-text_user_banned_from_private = 'user is banned'
+text_user_banned_from_private = 'User is banned'
 text_admin_try_ban_admin = '''Этот пользователь является администратором группы. Администраторов имеет право банить 
 только владелец группы'''
 text_banned_user_already_kicked = 'Этот пользователь уже забанен'
 text_banned_user_left = 'Этот пользователь вышел из группы'
+text_banned_user_is_creator = 'Вы не можете забанить владельца группы'
 
