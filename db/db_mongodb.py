bdc3e7a33e20a06cdba07fb0d40066351e0899a1
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-17 00:02:25 +0400

    fixup! refactor db_mongodb.py: add def get_users_by_role

--- db/db_mongodb.py
+++ db/db_mongodb.py
@@ -49,7 +49,7 @@ async def get_user_data(user_data_key: str, user_data_value) -> dict:
     return await db['users'].find_one({user_data_key: user_data_value})
 
 
-async def get_users_by_role(chat_id: int, role):
+async def get_users_by_role(chat_id: int, role) -> list:
     cursor = db['group_user_role'].find({'chat_id': chat_id, 'role': role})
     return await cursor.to_list(length=100)
 
