a962343c0b33d1f8f359f3c3666594f82b766a72
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-19 23:48:18 +0400

    refactor state.py: add waiting_message_for_mute_user

--- filter/state.py
+++ filter/state.py
@@ -4,3 +4,4 @@ from aiogram.filters.state import StatesGroup, State
 class MyState(StatesGroup):
     waiting_message_for_ban_user = State()
     waiting_message_for_unban_user = State()
+    waiting_message_for_mute_user = State()
