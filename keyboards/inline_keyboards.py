5d48e253404da6b85c9765e3ea4b266c07da9b7d
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-19 23:49:52 +0400

    refactor inline_keyboards.py: change callback_data in buttons Mute and Unmute in members_management_inline_keyboard

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -57,8 +57,8 @@ def members_management_inline_keyboard(chat_id: int, user_id: int):
     buttons = [
         [InlineKeyboardButton(text='Забанить участника', callback_data=f'BanUser_{user_id}_{chat_id}')],
         [InlineKeyboardButton(text='Разбанить участника', callback_data=f'UnbanUser_{user_id}_{chat_id}')],
-        [InlineKeyboardButton(text='Замьютить участника', callback_data=f'mute_user_{user_id}_{chat_id}')],
-        [InlineKeyboardButton(text='Размьютить участника', callback_data=f'unmute_user_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Замьютить участника', callback_data=f'MuteUser_{user_id}_{chat_id}')],
+        [InlineKeyboardButton(text='Размьютить участника', callback_data=f'UnmuteUser_{user_id}_{chat_id}')],
         [InlineKeyboardButton(text='Назад', callback_data=f'GrMan_{user_id}_{chat_id}')],
         [InlineKeyboardButton(text='К списку групп', callback_data=f'UpdGr_{user_id}')],
     ]
