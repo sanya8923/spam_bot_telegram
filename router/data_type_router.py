5e61d77131d83a072ae472ac4e1fac1dc177002d
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-22 00:33:39 +0400

    refactor data_type_router.py

--- router/data_type_router.py
+++ router/data_type_router.py
@@ -5,18 +5,17 @@ from db_manager.db_manager import DbManager
 class DataTypeRouter(MyRouter):
     def __init__(self, *args, **kwargs):
         self.data = args[0]
+        self.db_manager = DbManager()
 
     async def run(self):
         print('DataTypeRouter')
         try:
-            db_manager = DbManager()
-
             if self.data.type == 'user_data':
-                await db_manager.on_user_data(self.data)
+                await self.db_manager.on_user_data(self.data)
             elif self.data.type == 'message_data':
-                await db_manager.on_message_data(self.data)
+                await self.db_manager.on_message_data(self.data)
             elif self.data.type == 'group_data':
-                await db_manager.on_group_data(self.data)
+                await self.db_manager.on_group_data(self.data)
             else:
                 return False
         except TypeError as e:
