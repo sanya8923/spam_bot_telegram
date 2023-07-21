913029338453ab2921c87d69535e68846e1881c3
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-21 20:09:20 +0400

    refactor db_manager.py: add router (in work)

--- db_manager/db_manager.py
+++ db_manager/db_manager.py
@@ -1,2 +1,28 @@
 class DbManager:
-    pass
+    def __init__(self, *args, **kwargs):
+        self.data = args[0]
+
+    async def qualifier(self):
+        pass
+
+    async def on_user_data(self):
+        pass
+
+    async def on_group_data(self):
+        pass
+
+    async def on_message_data(self):
+        pass
+
+    async def router(self):
+        db_manager = DbManager(self.data)
+        try:
+            if db_manager == 'user':
+                await db_manager.on_user_data()
+            elif db_manager == 'group':
+                await db_manager.on_group_data()
+            elif db_manager == 'message':
+                await db_manager.on_message_data()
+        except None # TODO: add except
+
+
