ba1093d3e5ab52d549182868c80d2e195bcf5cc8
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 22:48:34 +0400

    add async def save_message in class DataConverterGroup

--- data_converter/data_convert_group.py
+++ data_converter/data_convert_group.py
@@ -1,9 +1,13 @@
 from data_converter import DataConverter
 from data.data import UserData, GroupData, MessageData
 from typing import Union
+from aiogram.types import Message
 
 
 class DataConverterGroup(DataConverter):
     async def get_data_to_dict(self, obj: GroupData) -> dict:
         print('get_data_to_dict in DataConverterGroup')
         return {'user': 18}
+
+    async def save_message(self, message: Message):
+        pass
