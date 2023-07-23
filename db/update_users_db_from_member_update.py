f8a134fba8f4b83e6a5086c21d1e6bcfa67f8221
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 21:37:49 +0400

    refactor update_users_db_from_member_update.py

--- db/update_users_db_from_member_update.py
+++ db/update_users_db_from_member_update.py
@@ -1,9 +1,10 @@
 from aiogram.types import ChatMemberUpdated
-from db.db_mongodb import add_data_to_db, db
+from db.db_mongodb import Db
 
 
 async def update_users_db_from_member_update(update: ChatMemberUpdated):
     print('update_users_db_from_member_update')
+    db = Db()
     collection_name = 'users'
     collection = db[collection_name]
     count = await collection.count_documents({'user_id': update.new_chat_member.user.id})
@@ -15,10 +16,10 @@ async def update_users_db_from_member_update(update: ChatMemberUpdated):
     }
 
     if count == 0:
-        await add_data_to_db(collection_name, user)
+        await db.add_data(collection_name, user)
     elif count > 1:
         delete_filter = {'user_id': update.new_chat_member.user.id}
         collection.delete_many(delete_filter)
 
-        await add_data_to_db(collection_name, user)
+        await db.add_data(collection_name, user)
 
