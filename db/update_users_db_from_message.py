b7d045e5382be27d541b8c651561c2ceb49eb42d
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 21:37:35 +0400

    refactor update_users_db_from_message.py

--- db/update_users_db_from_message.py
+++ db/update_users_db_from_message.py
@@ -1,24 +1,25 @@
 from aiogram.types import Message
-from db.db_mongodb import add_data_to_db, db
+from db.db_mongodb import Db
 
 
 async def update_users_db_from_message(message: Message):
     print('update_users_db_from_message')
-    collection_name = 'users'
-    collection = db[collection_name]
-    count = await collection.count_documents({'user_id': message.from_user.id})
-    user = {
-        'user_id': message.from_user.id,
-        'username': message.from_user.username,
-        'first_name': message.from_user.first_name,
-        'last_name': message.from_user.last_name
-    }
-
-    if count == 0:
-        await add_data_to_db(collection_name, user)
-    elif count > 1:
-        delete_filter = {'user_id': message.from_user.id}
-        collection.delete_many(delete_filter)
-
-        await add_data_to_db(collection_name, user)
+    # db = Db()
+    # collection_name = 'users'
+    # collection = db[collection_name]
+    # count = await collection.count_documents({'user_id': message.from_user.id})
+    # user = {
+    #     'user_id': message.from_user.id,
+    #     'username': message.from_user.username,
+    #     'first_name': message.from_user.first_name,
+    #     'last_name': message.from_user.last_name
+    # }
+    #
+    # if count == 0:
+    #     await db.add_data(collection_name, user)
+    # elif count > 1:
+    #     delete_filter = {'user_id': message.from_user.id}
+    #     collection.delete_many(delete_filter)
+    #
+    #     await db.add_data(collection_name, user)
 
