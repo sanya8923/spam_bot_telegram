e4e36d0ccc6f82f44fdaac9bf73bf8013079b48b
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 23:34:26 +0400

    finish save_message in data_converter_message.py (replace code from save message in data_manager.py)

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -5,6 +5,7 @@ from db_manager.db_manager import DbManager
 from data_converter.data_convert_group import DataConverterGroup
 from data_converter.data_converter_message import DataConverterMessage
 from data_converter.data_converter_users import DataConverterUsers
+from data_converter.data_converter import DataConverter
 
 
 class DataManager:
@@ -15,27 +16,9 @@ class DataManager:
     async def save_message(self, message: Message) -> MessageData:
         print('save_objects')
 
-        from_user = UserData()
-        from_user.id = message.from_user.id
-        from_user.username = message.from_user.username
-        from_user.first_name = message.from_user.first_name
-        from_user.last_name = message.from_user.last_name
+        await DataConverterMessage().save_message(message)
 
-        from_chat = GroupData()
-        from_chat.id = message.chat.id
-        from_chat.type = message.chat.type
-        from_chat.chat_username = message.chat.username
-        from_chat.chat_name = message.chat.title
 
-        message_data = MessageData()
-        message_data.id = message.message_id
-        message_data.text = message.text
-        message_data.entities_data = message.entities
-        message_data.entities_type = message.entities['type']
-        message_data.from_user = from_user
-        message_data.from_chat = from_chat
-
-        return message_data
 
     async def get_data_to_dict(self, obj: Union[list, UserData, GroupData, MessageData]):
         print('get_data_to_dict')
