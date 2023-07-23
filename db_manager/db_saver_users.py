7f45a8e3372abecafc593fa62da04868c6174bfd
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 21:33:12 +0400

    add db_saver_users.py and class DbSaverUsers

---
+++ db_manager/db_saver_users.py
@@ -0,0 +1,13 @@
+from db_saver import DbSaver
+from data.data import UserData, GroupData, MessageData
+from typing import Union
+from data_manager.data_manager import DataManager
+from db.db_mongodb import Db
+
+
+class DbSaverUsers(DbSaver):
+    async def save_to_db(self, obj: UserData) -> bool:
+        print('save_to_db in DbSaverUsers')
+        user_dict = await DataManager().set_data_to_dict(obj)
+        await Db().update_data_one(user_dict)
+
