b2b5ee676b2541d8a9d1e9be65d57843d154b550
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 21:38:22 +0400

    refactor save_group_to_db.py

--- db/save_group_to_db.py
+++ db/save_group_to_db.py
@@ -1,10 +1,11 @@
 from aiogram.types import ChatMemberUpdated
 
-from db.db_mongodb import db, add_data_to_db
+from db.db_mongodb import Db
 
 
 async def save_group_to_db(update: ChatMemberUpdated):
     print('save_group_to_db')
+    db = Db
     collection_name = 'groups'
     collection = db[collection_name]
 
@@ -15,5 +16,5 @@ async def save_group_to_db(update: ChatMemberUpdated):
             'chat_username': update.chat.username,
             'chat_name': update.chat.title
         }
-        await add_data_to_db(collection_name, group)
+        await db.add_data(collection_name, group)
 
