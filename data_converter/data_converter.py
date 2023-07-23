ccce97d352fcc83d53f3f5298aed1334e40e686a
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-23 22:44:23 +0400

    refactor args description in get_data_to_dict add async def save_message

--- data_converter/data_converter.py
+++ data_converter/data_converter.py
@@ -1,7 +1,14 @@
 from abc import ABC, abstractmethod
+from aiogram.types import Message
+from data.data import UserData, GroupData, MessageData
+from typing import Union
 
 
 class DataConverter(ABC):
     @abstractmethod
-    async def get_data_to_dict(self, obj: ):
+    async def get_data_to_dict(self, obj: Union[UserData, GroupData, MessageData]):
+        pass
+
+    @abstractmethod
+    async def save_message(self, message: Message):
         pass
