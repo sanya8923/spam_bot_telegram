853762311900813ce496d914b57dd4ca7f7bd28e
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-24 00:35:57 +0400

    delete save_message from DataConvertMessage in data_converter_message.py

--- data_converter/data_converter_message.py
+++ data_converter/data_converter_message.py
@@ -9,27 +9,3 @@ class DataConverterMessage(DataConverter):
         print('get_data_to_dict in DataConverterMessage')
         return {'user': 18}
 
-    async def save_message(self, message: Message):
-        print('save_message in DataConverterMessage')
-
-        from_user = UserData()
-        from_user.id = message.from_user.id
-        from_user.username = message.from_user.username
-        from_user.first_name = message.from_user.first_name
-        from_user.last_name = message.from_user.last_name
-
-        from_chat = GroupData()
-        from_chat.id = message.chat.id
-        from_chat.type = message.chat.type
-        from_chat.chat_username = message.chat.username
-        from_chat.chat_name = message.chat.title
-
-        message_data = MessageData()
-        message_data.id = message.message_id
-        message_data.text = message.text
-        message_data.entities_data = message.entities
-        message_data.entities_type = message.entities[0]  # TODO: check index
-        message_data.from_user = from_user
-        message_data.from_chat = from_chat
-
-        return message_data
