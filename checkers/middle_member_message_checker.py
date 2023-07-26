88a35b6d3aba9ba7ebe4e8c701f649f31d086051
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 23:48:37 +0400

    add prints

--- checkers/middle_member_message_checker.py
+++ checkers/middle_member_message_checker.py
@@ -6,10 +6,13 @@ class MiddleMemberMassageChecker(MessageChecker):
         self.member = member
 
     async def url_check(self):
+        print(f'url_check in MiddleMemberMassageChecker: {url_check}')
         pass
 
     async def flood_check(self):
+        print(f'flood_check in MiddleMemberMassageChecker: {flood_check}')
         pass
 
     async def ban_words_check(self):
+        print(f'ban_words_check in MiddleMemberMassageChecker:{ban_words_check}')
         pass
