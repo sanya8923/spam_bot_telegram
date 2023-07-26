8fe62dc854dab167b0d9d597afe0314f152f1b73
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 23:50:26 +0400

    add prints

--- checkers/new_member_message_checker.py
+++ checkers/new_member_message_checker.py
@@ -6,10 +6,13 @@ class NewMemberMessageChecker(MessageChecker):
         self.member = member
 
     async def url_check(self):
+        print(f'url_check in NewMemberMessageChecker')
         pass
 
     async def flood_check(self):
+        print(f'flood_check in NewMemberMessageChecker')
         pass
 
     async def ban_words_check(self):
+        print('ban_words_check in NewMemberMessageChecker')
         pass
