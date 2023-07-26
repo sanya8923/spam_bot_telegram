7f10ac1823aa8d1796bfe06f9e388dce9a5e79f6
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 22:18:16 +0400

    add __init__ and method check in class MessageManager in message_manager.py

--- object_manager/message_manager.py
+++ object_manager/message_manager.py
@@ -1,5 +1,10 @@
-from object_manager import ObjectManager
+from object_manager.object_manager import ObjManager
 
 
-class MessageManager(ObjectManager):
-    pass
+class MessageManager(ObjManager):
+    def __init__(self, obj):
+        self.object = obj
+
+    async def check(self):
+        print('check in MessageManager')
+        return 0
