d9dc547139922a592b6149d9c80ab6fe1ab5ae10
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 23:12:48 +0400

    does MessageChecker abstract

--- checkers/message_checker.py
+++ checkers/message_checker.py
@@ -1,15 +1,19 @@
 from aiogram.types import Message
+from abc import ABC, abstractmethod
 
 
-class MessageChecker:
+class MessageChecker(ABC):
     def __init__(self, message: Message):
         self.message = message
 
+    @abstractmethod
     async def url_check(self):
         pass
 
+    @abstractmethod
     async def flood_check(self):
         pass
 
+    @abstractmethod
     async def ban_words_check(self):
         pass
