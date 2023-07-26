00cd8fd196508b3fb5ed44b1e5877181cb5a4196
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 21:38:27 +0400

    add abstractmethod check in class ObjectManager

--- object_manager/object_manager.py
+++ object_manager/object_manager.py
@@ -2,7 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class ObjectManager(ABC):
-    def __init__(self, object):
-        self.object = object
+    def __init__(self, obj):
+        self.object = obj
 
-
+    @abstractmethod
+    async def check(self):
+        pass
