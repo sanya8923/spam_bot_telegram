9d75beb001f2a9358a09c8b2606987fbd951cfed
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 22:42:39 +0400

    rename arg obj -> message in class MessageManager in message_manager.py

--- object_manager/message_manager.py
+++ object_manager/message_manager.py
@@ -1,10 +1,12 @@
 from object_manager.object_manager import ObjManager
+from aiogram.types import Message
 
 
 class MessageManager(ObjManager):
-    def __init__(self, obj):
-        self.object = obj
+    def __init__(self, message: Message):
+        self.message = message
 
     async def check(self):
         print('check in MessageManager')
+        url = self.message
         return 0
