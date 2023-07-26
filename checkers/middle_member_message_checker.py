4e3be83fc22d134295d50c440b5aa95295e71565
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 23:42:21 +0400

    rename class MiddleMemberChecker -> MiddleMemberMassageChecker

--- checkers/middle_member_message_checker.py
+++ checkers/middle_member_message_checker.py
@@ -1,7 +1,7 @@
 from checkers.message_checker import MessageChecker
 
 
-class MiddleMemberChecker(MessageChecker):
+class MiddleMemberMassageChecker(MessageChecker):
     def __init__(self, member):
         self.member = member
 
