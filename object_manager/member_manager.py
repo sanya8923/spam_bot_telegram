50bbfd55a1563622025c922943fccb7fc720a223
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 22:17:50 +0400

    add __init__ and method check in class MemberManager in member_manager.py

--- object_manager/member_manager.py
+++ object_manager/member_manager.py
@@ -1,5 +1,10 @@
-from object_manager import ObjectManager
+from object_manager.object_manager import ObjManager
 
 
-class MemberManager(ObjectManager):
-    pass
+class MemberManager(ObjManager):
+    def __init__(self, obj):
+        self.object = obj
+
+    async def check(self):
+        print('check in MemberManager')
+        pass
