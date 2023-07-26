6d550efc09ec8196563e69f3809ad62820a8f21c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-26 23:55:59 +0400

    refactor async def check in class MessageManager in message_manager.py

--- object_manager/message_manager.py
+++ object_manager/message_manager.py
@@ -2,24 +2,35 @@ from object_manager.object_manager import ObjManager
 from aiogram.types import Message
 from checkers.message_checker import MessageChecker
 from member_manager import MemberManager
-from checkers.new_member_message_checker import NewMemberChecker
-from checkers.middle_member_message_checker import MiddleMemberChecker
+from checkers.new_member_message_checker import NewMemberMessageChecker
+from checkers.middle_member_message_checker import MiddleMemberMassageChecker
 
 
 class MessageManager(ObjManager):
     def __init__(self, message: Message):
         self.message = message
+        self.member = self.message.from_user
 
     async def check(self):
         print('check in MessageManager')
-        member = self.message.from_user
-        member_manager = MemberManager(member)
+        member_manager = MemberManager(self.member)
         member_status = await member_manager.get_status_member()
-        message_checker = MessageChecker(self.message)
-
-        if member_status == 'new_member':
-            new_member_checker = NewMemberChecker()
-        else:
-            pass
-
-        return 0
+        try:
+            if member_status == 'new_member':
+                message_checker = NewMemberMessageChecker(self.message)
+                if await message_checker.url_check() or \
+                        await message_checker.ban_words_check() or \
+                        await message_checker.flood_check():
+                    return True
+                else:
+                    return False
+            else:
+                message_checker = MiddleMemberMassageChecker(self.message)
+                if await message_checker.url_check() or \
+                        await message_checker.ban_words_check() or \
+                        await message_checker.flood_check():
+                    return True
+                else:
+                    return False
+        except (ValueError, TypeError) as e:
+            raise (ValueError, TypeError)
