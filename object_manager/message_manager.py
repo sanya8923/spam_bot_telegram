9c9f92272c72476d0d19794116b7e9de64396d81
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-27 20:20:56 +0400

    refactor method check in class MessageManager in message_manager.py

--- object_manager/message_manager.py
+++ object_manager/message_manager.py
@@ -1,6 +1,6 @@
 from object_manager.object_manager import ObjManager
 from aiogram.types import Message
-from member_manager import MemberManager
+from object_manager.member_manager import MemberManager
 from checkers.new_member_message_checker import NewMemberMessageChecker
 from checkers.middle_member_message_checker import MiddleMemberMassageChecker
 
@@ -9,10 +9,11 @@ class MessageManager(ObjManager):
     def __init__(self, message: Message):
         self.message = message
         self.member = self.message.from_user
+        self.group = self.message.chat
 
     async def check(self):
         print('check in MessageManager')
-        member_manager = MemberManager(self.member)
+        member_manager = MemberManager(self.member, self.group)
         member_status = await member_manager.get_status_member()
         try:
             if member_status == 'new_member':
