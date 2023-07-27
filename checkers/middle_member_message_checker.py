ff4ad94a80402c5091eb6beeeda40bed537ec4b2
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-27 19:58:49 +0400

    delete all methods from MiddleMemberMassageChecker

--- checkers/middle_member_message_checker.py
+++ checkers/middle_member_message_checker.py
@@ -4,15 +4,4 @@ from checkers.message_checker import MessageChecker
 class MiddleMemberMassageChecker(MessageChecker):
     def __init__(self, member):
         self.member = member
-
-    async def url_check(self):
-        print(f'url_check in MiddleMemberMassageChecker')
-        pass
-
-    async def flood_check(self):
-        print(f'flood_check in MiddleMemberMassageChecker')
-        pass
-
-    async def ban_words_check(self):
-        print(f'ban_words_check in MiddleMemberMassageChecker')
-        pass
+        print('MiddleMemberMassageChecker')
