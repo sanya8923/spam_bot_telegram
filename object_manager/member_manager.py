643ec889e7690da1ad88ef4d77fcacaf8e2b8bae
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-07-27 20:19:43 +0400

    finished method get_status_member in class MemberManager in member_manager.py

--- object_manager/member_manager.py
+++ object_manager/member_manager.py
@@ -1,14 +1,18 @@
 from object_manager.object_manager import ObjManager
+from bot import bot
+from aiogram.types import TelegramObject
 
 
 class MemberManager(ObjManager):
-    def __init__(self, member):
+    def __init__(self, member: TelegramObject, group: TelegramObject):
         self.member = member
+        self.group = group
 
     async def check(self):
         print('check in MemberManager')
         pass
 
-    async def get_status_member(self):
+    async def get_status_member(self) -> str:
         print('get_status_member in MemberManager')
-        pass
+        member = await bot.get_chat_member(self.group.id, self.member.id)
+        return member.status
